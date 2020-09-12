a2d2a842f994ac4b6b3aee4dd4fbea9738ca3502
Naveen M K <naveen@syrusdark.website>
2020-09-12 20:28:17 +0530

    Remove unnecessary

--- final/final_comile_all.py
+++ final/final_comile_all.py
@@ -20,26 +20,5 @@ class ManimLogo(Scene):
         for i in temp_dict:
             self.add(temp_dict[i])
         self.wait(3)
-    def some(self):
-        m_text=ImageMobject("m_logo.png").scale(4)
-        self.play(FadeIn(m_text),run_time=3)
-        self.wait(2)
-        self.remove(m_text)
-        
-        a_text=ImageMobject("a_logo.png").scale(4)
-        self.play(FadeIn(a_text),run_time=3)
-        self.remove(a_text)
-        self.wait(1)
-        n_text=ImageMobject("n_logo.png").scale(4)
-        self.play(FadeIn(n_text),run_time=3)
-        self.remove(n_text)
-        self.wait(2)
-        i_text=ImageMobject("i_logo.png").scale(4)
-        self.play(FadeIn(i_text),run_time=3)
-        self.remove(i_text)
-        self.wait(2)
-        m_text=ImageMobject("m_logo.png").scale(4)
-        self.play(FadeIn(m_text),run_time=3)
-        self.remove(m_text)
-        self.wait(2)
+
 "manim final_comile_all.py -pl -r 500,500 --disable_caching"
